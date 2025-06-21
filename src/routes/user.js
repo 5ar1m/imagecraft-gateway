d68031bcecd40ff2b5b4e8f5bc5db104285d0965
5ar1m <2022umt1495@gmail.com>
2025-06-22 00:51:04 +0530

    remove user routes
    removed GET / and PATCH /update user routes

--- src/routes/user.js
+++ src/routes/user.js
@@ -1,20 +1,12 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/', (req, res) => {
-  res.send('user details');
-});
-
 router.post('/signin', (req, res) => {
   res.send('user created');
 });
 
 router.post('/login', (req, res) => {
     res.send('user logged in');
-});
-
-router.patch('/update', (req, res) => {
-    res.send('user data updated');
 });
 
 module.exports = router;
